84c59c9d26165d82ded0d145f6832ae4b69373c6
João Pedro Mattos <joao_pedro_mattos@hotmail.com>
2021-06-05 20:20:27 -0300

    Adding comments...

--- notebooks/resnet.py
+++ notebooks/resnet.py
@@ -2,44 +2,6 @@ import haiku as hk
 import jax
 import jax.numpy as jnp
 import numpy as np
-
-
-# class ResNet(nn.Module):
-#     def __init__(self, block, num_blocks, num_classes=10):
-#         super(ResNet, self).__init__()
-#         self.in_planes = 64
-
-#         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-#                                stride=1, padding=1, bias=False)
-#         self.bn1 = nn.BatchNorm2d(64)
-#         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-#         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-#         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-#         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-#         self.linear = nn.Linear(512*block.expansion, num_classes)
-
-#     def _make_layer(self, block, planes, num_blocks, stride):
-#         strides = [stride] + [1]*(num_blocks-1)
-#         layers = []
-#         for stride in strides:
-#             layers.append(block(self.in_planes, planes, stride))
-#             self.in_planes = planes * block.expansion
-#         return nn.Sequential(*layers)
-
-#     def forward(self, x):
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = self.layer1(out)
-#         out = self.layer2(out)
-#         out = self.layer3(out)
-#         out = self.layer4(out)
-#         out = F.avg_pool2d(out, 4)
-#         out = out.view(out.size(0), -1)
-#         out = self.linear(out)
-#         return out
-
-
-# -------------------------------
-
 class BasicBlock(hk.Module):
     expansion = 1
 
@@ -53,6 +15,9 @@ class BasicBlock(hk.Module):
         self.bn2 = hk.BatchNorm2d(planes)
 
         self.shortcut = hk.Sequential()
+
+        # I didn't figure it out yet what self.expansion does in pytorch
+        # thus I didn't translate it into jax...
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = hk.Sequential([
                 hk.Conv2d(in_planes, self.expansion*planes,
@@ -80,6 +45,8 @@ class ResNet(hk.Module):
         self.conv1 = hk.Conv2d(64, kernel_shape=3, stride=1,
                                padding='SAME', bias=False)  # OK
         self.bn1 = hk.BatchNorm2d()
+
+        # I'll leave these lines commented just because _make_layer is not working
         # self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         # self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         # self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
@@ -101,7 +68,7 @@ class ResNet(hk.Module):
         # out = self.layer2(out)
         # out = self.layer3(out)
         # out = self.layer4(out)
-        out = hk.avg_pool2d(out, 4)
+        out = hk.avg_pool(out, 4) # This is probably wrong.
         out = hk.Reshape(output_shape=(-1, out.size(0)))(out)
         out = self.linear(out)
         return out
