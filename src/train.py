07eb80e877244d7a3a0c0bd3cf40fa64ccc17263
João Pedro Rodrigues Mattos <joao_pedro_mattos@hotmail.com>
2021-06-24 19:45:06 -0300

    lp_path_norm tested (it also works...)!

--- src/train.py
+++ src/train.py
@@ -38,9 +38,9 @@ def l2_loss(params):
     return 0.5 * sum(jnp.sum(jnp.square(p)) for p in params)
 
 def lp_path_norm(forward, weights, p=2, input_size=[3, 32, 32]):
-    pw_model = jax.tree_map(lambda w: jnp.pow(jnp.abs(w), p), weights)
+    pw_model = jax.tree_map(lambda w: jnp.power(jnp.abs(w), p), weights)
     data_ones = jnp.ones(input_size)
-    return (forward.apply(params, state, None, data_ones).sum() ** (1 / p ))
+    return (forward.apply(pw_model, state, data_ones).sum() ** (1 / p ))
 
 class TrainState(NamedTuple):
     params: hk.Params
